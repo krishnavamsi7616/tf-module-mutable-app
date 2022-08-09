43f21377c11fd9528afb307f028ce4383feed145
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-08 23:29:55 -0700

    updating ./terraform/tf-module-mutable-app

--- target-group.tf
+++ target-group.tf
@@ -10,8 +10,9 @@ resource "aws_lb_target_group" "target-group" {
     enabled = true
     healthy_threshold = 2
     unhealthy_threshold = 2
-    interval = 5
+    interval = 6
     path = "/health"
+    timeout = 5
   }
 }
 
