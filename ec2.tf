491a6d745f318734eed5af8175406bfbbf783677
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-16 21:34:37 -0700

    updating ./terraform/tf-module-mutable-app

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,7 @@
-resource "aws_spot_instance_request" "instance" {
+resource "aws_instance" "instance" {
   count                  = var.INSTANCE_COUNT
   ami                    = data.aws_ami.ami.image_id
-  spot_price             = data.aws_ec2_spot_price.spot_price.spot_price
   instance_type          = var.INSTANCE_TYPE
-  wait_for_fulfillment   = true
   subnet_id              = var.PRIVATE_SUBNET_IDS[0]
   vpc_security_group_ids = [aws_security_group.main.id]
   iam_instance_profile   = aws_iam_instance_profile.allow-secret-manager-read-access.name
@@ -15,14 +13,14 @@ resource "aws_spot_instance_request" "instance" {
 
 resource "aws_ec2_tag" "name-tag" {
   count       = var.INSTANCE_COUNT
-  resource_id = aws_spot_instance_request.instance.*.spot_instance_id[count.index]
+  resource_id = aws_instance.instance.*.id[count.index]
   key         = "Name"
   value       = local.TAG_PREFIX
 }
 
 resource "aws_ec2_tag" "monitor-tag" {
   count       = var.INSTANCE_COUNT
-  resource_id = aws_spot_instance_request.instance.*.spot_instance_id[count.index]
+  resource_id = aws_instance.instance.*.id[count.index]
   key         = "Monitor"
   value       = "yes"
 }
@@ -33,7 +31,7 @@ resource "null_resource" "ansible" {
     connection {
       user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
       password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASS"]
-      host     = aws_spot_instance_request.instance.*.private_ip[count.index]
+      host     = aws_instance.instance.*.private_ip[count.index]
     }
     inline = [
       "ansible-pull -U https://github.com/krishnavamsi7616/roboshop-ansible.git roboshop.yml -e HOST=localhost -e ROLE=${var.COMPONENT} -e ENV=${var.ENV} -e DOCDB_ENDPOINT=${var.DOCDB_ENDPOINT} -e REDIS_ENDPOINT=${var.REDIS_ENDPOINT} -e MYSQL_ENDPOINT=${var.MYSQL_ENDPOINT}",
