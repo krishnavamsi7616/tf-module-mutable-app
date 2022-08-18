68f659f7c529a1ece3ff12e3d9c2be5c0a61e891
Krishna Vamsi <nvgkvamsi@gmail.com>
2022-08-17 17:55:05 -0700

    updating ./terraform/tf-module-mutable-app

--- ec2.tf
+++ ec2.tf
@@ -26,6 +26,9 @@ resource "aws_ec2_tag" "monitor-tag" {
 }
 
 resource "null_resource" "ansible" {
+  triggers = {
+    abc = timestamp()
+  }
   count        = var.INSTANCE_COUNT
   provisioner "remote-exec" {
     connection {
